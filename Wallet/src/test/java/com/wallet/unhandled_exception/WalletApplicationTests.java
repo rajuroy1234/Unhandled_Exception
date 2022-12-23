8a1cc3c36088be9e566e0e59fba1ee65d147797e
Raju <rajuroy258500@gmail.com>
2022-12-23 16:36:27 +0530

    Added signing and verification apis

---
+++ Wallet/src/test/java/com/wallet/unhandled_exception/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.unhandled_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
